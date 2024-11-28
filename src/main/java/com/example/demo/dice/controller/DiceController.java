da64c2831ccec0f02a670ba03397601f5fb4b203
Kyeongmin Lee <124436145+2kilometer@users.noreply.github.com>
2024-11-28 17:03:45 +0900

    feat: [이경민] 랜덤 주사위 굴리기 [SBwD-1]

---
+++ src/main/java/com/example/demo/dice/controller/DiceController.java
@@ -0,0 +1,33 @@
+package com.example.demo.dice.controller;
+
+import com.example.demo.dice.entity.Dice;
+import com.example.demo.dice.service.DiceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/dice")
+public class DiceController {
+    final private DiceService diceService;
+
+    @GetMapping("/test")
+    public String diceTest() {
+        log.info("diceTest() called");
+
+        return "diceTest() called";
+    }
+
+    @GetMapping("/roll-dice")
+    public Dice rollDice() {
+        log.info("rollDice() called");
+
+        Dice acquiredDice = diceService.rollDice();
+
+        return acquiredDice;
+    }
+}
